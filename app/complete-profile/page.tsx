258b43296cc5a6c3b6560af29db9ed501759e057
Jesse Nwanni <mastakonnect@gmail.com>
2025-07-09 00:43:19 +0100

    Update page.tsx

--- app/complete-profile/page.tsx
+++ app/complete-profile/page.tsx
@@ -244,13 +244,6 @@ export default function CompleteProfilePage() {
                   <span className="text-[#CB0207] font-bold text-xl">Strapre</span>
                 </div>
               </div>
-
-              
-            <Link href="#">
-                <Button className=" bg-[#CB0207] hover:bg-[#A50206] text-white text-[10px] md:text-[12px] px-6 py-3 rounded-xl font-medium shadow-lg hover:shadow-xl transition-all duration-300">
-                  LOGIN / REGISTER
-                </Button>
-              </Link>
           </div>
         </div>
       </header>
