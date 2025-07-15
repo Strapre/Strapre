48211d13ed00a0fd5b43d992890bef441a13437d
Jesse Nwanni <mastakonnect@gmail.com>
2025-07-15 07:25:33 +0100

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "Strapre-v1"
-const urlsToCache = ["/", "/manifest.json", "/icon-192x192.png", "/icon-512x512.png"]
+const urlsToCache = ["/", "/manifest.json", "/android/android-launchericon-192-192.png", "/android/android-launchericon-512-512.png"]
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)))
