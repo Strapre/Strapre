9ea70e9d7af07aead103be14383eceba96fa1cb7
Jesse Nwanni <mastakonnect@gmail.com>
2025-07-16 18:05:45 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ['ga.vplaza.com.ng'], 
+    domains: ['api.strapre.com'], 
     formats: ['image/webp'], 
   },
 }
