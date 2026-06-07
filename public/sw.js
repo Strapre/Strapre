e442b9701a6d26f9d8e7ad3d3cec5cac537d72e4
Anelsonmousse <samueldickson06@gmail.com>
2026-06-07 11:30:28 +0100

    admin

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,12 @@ const CACHE_NAME = "Strapre-v1"
 const urlsToCache = ["/", "/manifest.json", "/android/android-launchericon-192-192.png", "/android/android-launchericon-512-512.png"]
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting()
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)))
+})
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim())
 })
 
 self.addEventListener("fetch", (event) => {
